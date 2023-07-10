895b2893725cf141103266a404db11705aea924f
prostraction <47314760+prostraction@users.noreply.github.com>
2023-07-11 00:52:00 +0300

    Customized design

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Rounter, Routes, Route } from "react-router-dom";
 import Menu from "./menu.jsx";
 import View from "./viewDS.jsx";
 import AddDS from "./addDS.jsx";
+import SignIn from "./signIn.jsx";
 
 export class App extends React.Component {
 
@@ -14,6 +15,7 @@ export class App extends React.Component {
               <Route exact path='/' element={<Menu></Menu>}></Route>
               <Route exact path='/set/:name' element={<><Menu></Menu><View></View></>}></Route>
               <Route exact path='/add' element={<><Menu></Menu><AddDS></AddDS></>}></Route>
+              <Route exact path='/auth' element={<SignIn></SignIn>}></Route>
             </Routes>
         </div>
       </Rounter>
---
+++ src/components/signIn.jsx
@@ -0,0 +1,22 @@
+import * as React from "react"
+
+export default class SignIn extends React.Component {
+
+    render() {
+        return(
+            <div className="view">
+                <div className="auth">
+                    <p><strong>Авторизация</strong></p>
+                    <form>
+                        <ul>
+                        <li><input placeholder="Логин"></input></li>
+                        <li><input placeholder="Пароль"></input></li>
+                        <button>Войти</button>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+
